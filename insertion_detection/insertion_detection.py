9bc0fbdd454ba846f20987b444dc94db731b0ef4
nahanaoo <ericsven.u@gmail.com>
2022-01-21 13:55:08 +0100

    working test version

--- insertion_detection/insertion_detection.py
+++ insertion_detection/insertion_detection.py
@@ -1,19 +1,42 @@
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 from os.path import join
-from os import listdir
-from subprocess import call, DEVNULL, STDOUT
+from subprocess import call, run as r, DEVNULL, STDOUT
+from io import StringIO
+import pandas as pd
+from plotting import plot_alignment
 
 
 class Insertion():
     def __init__(self):
+        self.out_dir = 'test_data'
         self.step = 10000
         self.window = 50000
-        mutant = 'test_data/assembly.gbk'
+        self.mutant_gbk = 'test_data/assembly.gbk'
+        self.mutant_contigs = [contig for contig in SeqIO.parse(
+            self.mutant_gbk, 'genbank')]
+        self.mutant_fasta = join(self.out_dir, 'mutant.fasta')
+        with open(join(self.out_dir, 'mutant.fasta'), 'w') as handle:
+            SeqIO.write(self.mutant_contigs, handle, 'fasta')
         self.reference = 'test_data/reference.fasta'
-        self.contigs = [contig for contig in SeqIO.parse(
-            mutant, 'genbank')]
-        self.out_dir = 'test_data'
+        self.ref_contigs = [contig for contig in SeqIO.parse(
+            self.reference, 'fasta')]
+        self.insertions = None
+        self.genbank = None
+        self.bam = join(self.out_dir, "aligned.sorted.bam")
+        self.annotated = pd.DataFrame(
+            columns=['chromosome', 'position', 'length', 'start', 'end', 'protein'])
+
+    def parse_genbank(self):
+        self.genbank = {contig.id: {} for contig in self.mutant_contigs}
+        for contig in self.mutant_contigs:
+            for feature in contig.features:
+                try:
+                    start = feature.location.start
+                    end = feature.location.end
+                    product = feature.qualifiers['product']
+                    self.genbank[contig.id][(start, end)] = product[0]
+                except KeyError:
+                    pass
 
     def chunker(self, seq, window_size, step):
         """Creates chunks of a sequence. window_size defines
@@ -38,7 +61,7 @@ class Insertion():
         """Chunks an assembly of multiple contigs into different 
         chunks using a sliding window algorightm (see chunker function)."""
         assembly_chunks = []
-        for contig in self.contigs:
+        for contig in self.ref_contigs:
             # Creates chunks of every contig
             assembly_chunks += self.chunker(contig, self.window, self.step)
         target = join(self.out_dir,
@@ -53,29 +76,74 @@ class Insertion():
         Minimap2 settings are set to accureate PacBio reads."""
         reads = join(self.out_dir, "chunked_sequences.fasta")
         sam = join(self.out_dir, "aligned.sam")
-        bam = join(self.out_dir, "aligned.sorted.bam")
         cmd = [
             "minimap2",
+            "--secondary=no",
             "-ax",
             "asm5",
-            self.reference,
+            self.mutant_fasta,
             reads,
             ">",
             sam,
         ]
+        # Calling minimap and surpressing stdout
+        call(" ".join(cmd), shell=True, stdout=DEVNULL,
+             stderr=STDOUT)
+        cmd = ['samtools', 'sort', '-o', self.bam, sam]
         # Calling samtools and surpressing stdout
         call(" ".join(cmd), shell=True, stdout=DEVNULL,
              stderr=STDOUT)
-        cmd = ['samtools', 'sort', '-o', bam, sam]
-        # Calling minimap and surpressing stdout
-        call(" ".join(cmd), shell=True, stdout=DEVNULL,
-             stderr=STDOUT)
-        cmd = ['samtools','index',bam]
+        cmd = ['samtools', 'index', self.bam]
         # Calling minimap and surpressing stdout
         call(" ".join(cmd), shell=True, stdout=DEVNULL,
              stderr=STDOUT)
 
+    def get_insertions(self):
+        cmd = ['samtools', 'depth', '-aa', '-J', '-Q', '60', self.bam]
+        process = r(cmd, capture_output=True)
+        df = pd.read_csv(StringIO(process.stdout.decode()), sep='\t')
+        df.columns = ['chromosome', 'position', 'coverage']
+        df = df[df['coverage'] == 0]
+        self.insertions = self.concat_insertions(df)
+        self.insertions['position'] = self.insertions['position'] - 1
+        self.insertions.to_csv(join(self.out_dir, 'insertions.tsv'), sep='\t')
+
+    def concat_insertions(self, df):
+        out = pd.DataFrame(columns=['chromosome', 'position', 'length'])
+        i = -1
+        prev_pos = 0
+        prev_contig = None
+        for contig, pos in zip(df['chromosome'], df['position']):
+            if (prev_contig == contig) & (pos - 1 == prev_pos):
+                out.at[i, 'length'] += 1
+            else:
+                i += 1
+                out.loc[i] = [contig, pos, 1]
+            prev_pos = pos
+            prev_contig = contig
+        return out
+
+    def annotate(self):
+        self.parse_genbank()
+        i = 0
+        for counter, row in self.insertions.iterrows():
+            c = row['chromosome']
+            p = row['position']
+            l = row['length']
+            for (start, end), product in self.genbank[c].items():
+                if (start >= p) & (end <= p+l):
+                    self.annotated.loc[i] = [c, p, l, start, end, product]
+                    i += 1
+        self.annotated.to_csv(
+            join(self.out_dir, 'insertions.annotaed.tsv'), sep='\t')
+
+    def plot_insertions(self):
+        for chromosome, position in zip(self.insertions['chromosome'], self.insertions['position']):
+            target = join(self.out_dir,'.'.join([chromosome,str(position),'pdf']))
+            plot_alignment(self.bam,chromosome,position,target)
 
 i = Insertion()
-i.chunk_assembly()
-i.mapper()
+# i.mapper()
+no_cov = i.get_insertions()
+i.annotate()
+i.plot_insertions()
--- insertion_detection/plotting.py
+++ insertion_detection/plotting.py
@@ -1,32 +1,40 @@
-from turtle import st
 from dna_features_viewer import GraphicFeature, GraphicRecord
 import pysam
 
-chromosome = 'AGTU001.0001.c02'
-start = 100000
-end = 300000
-bam = 'test_data/aligned.sorted.bam'
-a = pysam.AlignmentFile(bam)
-c = pysamstats.load_coverage(bam, chrom='Pf3D7_01_v3', pad=True)
-reads = []
 
-for read in a.fetch(chromosome, start, end):
-    reads.append(read)
-
-features = []
-for read in reads:
-    if read.is_reverse:
-        strand = 1
+def plot_alignment(bam, chromosome, position, out):
+    a = pysam.AlignmentFile(bam)
+    padding = 50000
+    if position - padding < 0:
+        start = 0
     else:
-        strand = -1
-    gf = GraphicFeature(start=read.reference_start, end=read.reference_end, strand=strand,
-                        color="#ffd700", label=read.query_name)
-    features.append(gf)
-record = GraphicRecord(sequence_length=end, features=features)
-record = record.crop((start, end))
+        start = position - padding
 
-record.plot_on_multiple_pages("chunks.pdf",
-                              nucl_per_line=end-start,
-                              lines_per_page=10,
-                              plot_sequence=False
-                              )
+    ref_length = a.get_reference_length(chromosome)
+    if position + padding > ref_length:
+        end = ref_length
+    else:
+        end = position + padding
+
+    reads = []
+    for read in a.fetch(chromosome, start, end):
+        if (not read.is_unmapped) & (not read.is_secondary) & (read.mapq == 60):
+            reads.append(read)
+
+    features = []
+    for read in reads:
+        if read.is_reverse:
+            strand = 1
+        else:
+            strand = -1
+        gf = GraphicFeature(start=read.reference_start, end=read.reference_end, strand=strand,
+                            color="#ffd700", label=read.query_name)
+        features.append(gf)
+    record = GraphicRecord(sequence_length=end, features=features)
+    record = record.crop((start, end))
+
+    record.plot_on_multiple_pages(out,
+                                  nucl_per_line=end-start,
+                                  lines_per_page=10,
+                                  plot_sequence=False
+                                  )
