ad7f8bec724e3209b8717d0ccdd8f9b7dd6e4485
nahanaoo <ericsven.u@gmail.com>
2022-03-11 16:30:55 +0100

    latest caller

--- insertion_detection/main.py
+++ insertion_detection/main.py
@@ -3,15 +3,15 @@ from .insertion_detection import Insertion
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Detect insertions in evolved bacterial strains. Fast and simpple.')
+        description='Detect insertions in evolved bacterial strains.')
     parser.add_argument(
-        'anceteral', help='fasta file of the ancesteral strain'
+        'ancestral', help='fasta file of the ancestral strain'
     )
     parser.add_argument(
-        'mutant', help='genbank file of the mutated strain.')
+        'mutant', help='genbank file of the mutated strain')
     parser.add_argument('out_dir', help='output directory')
     parser.add_argument('--plot', help='if this flag is added the alignment of every insertion\
-        is plotted.', action='store_true')
+        is plotted', action='store_true')
 
     return parser.parse_args()
 
@@ -22,6 +22,7 @@ def main():
     i.mapper()
     i.get_insertions()
     i.annotate()
+    i.dump()
     if args.plot:
         i.plot_insertions()
         i.plot_annotation()
