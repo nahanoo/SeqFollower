d72942b60a0f0bde1e06c7142e361c83d3b59efd
nahanaoo <ericsven.u@gmail.com>
2022-03-16 14:16:24 +0100

    fixed typos

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='insertion_detection',
       version='1.0',
-      description="Detects and visualizes large insertions in mutated bacterial strains.",
+      description="Detects insertions from assemblies of evolved bacterial strains.",
       author='Eric Ulrich',
       url='https://github.com/nahanoo/insertion_detection',
       packages=['insertion_detection'],
